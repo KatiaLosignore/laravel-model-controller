9b074a99b0520658eaa69d5eee9499f2ebe9868d
KatiaLosignore <katialos.kl@gmail.com>
2023-05-11 17:18:27 +0200

    added fix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,10 @@
 
     <nav class="navbar bg-primary bg-body-tertiary">
         <div class="container-fluid">
-            <div class="navbar-brand mb-0 h1 text-white">The Movies</div>
+            <div class="navbar-brand mb-0 h1">
+                <a href="{{route('home')}}">The Movies</a>
+            </div>
+            
         </div>
     </nav>
 
--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -8,7 +8,7 @@
         <div class="card container" style="width: 18rem;">
             <div class="card-body">
                 <h3 class="card-title">Title: {{$movie->title}}</h3>
-                <h3 class="card-text">Original Title:{{$movie->original_title}}</h3>
+                <h3 class="card-text">Original Title: {{$movie->original_title}}</h3>
                 <h4 class="card-text">Nationality: {{$movie->nationality}}</h4>
                 <p class="card-text mt-3 mb-4">Date: {{$movie->date}}</p>
                 <a href="#" class="btn btn-primary">Vote: {{$movie->vote}}</a>
